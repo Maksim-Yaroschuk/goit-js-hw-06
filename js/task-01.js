79a128387e83ca0f683d1a9c9224d6f0dcf169ea
Maksim-Yaroschuk <79583144+Maksim-Yaroschuk@users.noreply.github.com>
2022-05-16 21:55:48 +0300

    add task #2

--- js/task-01.js
+++ js/task-01.js
@@ -7,23 +7,3 @@ const categoryEl = categoriesEl.forEach(category => {
     console.log('Elements:', category.lastElementChild.childElementCount)
     console.log('')
 })
-
-
-// console.log('')
-// console.log('Другий варіант')
-// // Другий варіант
-// const categorEl = document.querySelector('#categories')
-// console.log('Number of categories 2.0:', categorEl.childElementCount)
-// console.log(categorEl.children)
-
-// const cat1 = document.querySelector('#categories')
-// console.log(cat1.querySelectorAll('.item'))
-
-// const categoryFirstEl = document.querySelector('.item')
-// console.log('Category:', categoryFirstEl.firstElementChild.textContent)
-// console.log('Elements:', categoryFirstEl.querySelectorAll('li').length)
-
-// console.log(categoryFirstEl.lastElementChild.textContent)
-
-// const cat = document.querySelectorAll('.item')
-// console.log(cat)
